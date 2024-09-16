92e7f451f20106518f47bc414b467a6d47eef705
Frank S. Thomas <frank@timepit.eu>
2024-09-16 08:18:33 +0200

    faulty DB2

---
+++ src/main/scala/prinzipien/db/DB2.scala
@@ -0,0 +1,99 @@
+package prinzipien.db
+
+import prinzipien.db.DB2.{FlatMap, HandleErrorWith, Pure}
+
+sealed trait DB2[+A] {
+  final def flatMap[B](f: A => DB2[B]): DB2[B] =
+    FlatMap(this, f)
+
+  final def map[B](f: A => B): DB2[B] =
+    flatMap(a => Pure(f(a)))
+
+  final def handleErrorWith[B](f: Throwable => DB2[B]): DB2[B] =
+    HandleErrorWith(this, f)
+}
+
+object DB2 {
+  final case class Get(key: String) extends DB2[Option[Int]]
+  final case class Put(key: String, value: Int) extends DB2[Unit]
+
+  final case class Pure[A](a: A) extends DB2[A]
+  final case class FlatMap[A, B](fa: DB2[A], f: A => DB2[B]) extends DB2[B]
+
+  final case class RaiseError[A](e: Throwable) extends DB2[A]
+  final case class HandleErrorWith[A, B](fa: DB2[A], f: Throwable => DB2[B]) extends DB2[B]
+
+  val unit: DB2[Unit] = Pure(())
+
+  def runDB[A](dbA: DB2[A]): A = {
+    def loop(
+        curr: DB2[Any],
+        valueStack: List[Any => DB2[Any]],
+        dropCount: Int,
+        errorStack: List[Throwable => DB2[Any]],
+        error: Option[Throwable],
+        db: Map[String, Int]
+    ): Any = {
+      error match {
+        case Some(e) =>
+          errorStack match {
+            case Nil    => throw e
+            case h :: t => loop(h(e), valueStack.drop(dropCount), 0, t, None, db)
+          }
+        case None =>
+          curr match {
+            case FlatMap(fa, f)         => loop(fa, f :: valueStack, dropCount + 1, errorStack, None, db)
+            case RaiseError(e)          => loop(DB2.unit, valueStack, dropCount, errorStack, Some(e), db)
+            case HandleErrorWith(fa, f) => loop(fa, valueStack, 0, f :: errorStack, None, db)
+            case _ =>
+              val v = curr match {
+                case Get(key)              => db.get(key)
+                case Put(_, _)             => ()
+                case Pure(a)               => a
+                case FlatMap(_, _)         => sys.error("impossible")
+                case RaiseError(_)         => sys.error("impossible")
+                case HandleErrorWith(_, _) => sys.error("impossible")
+              }
+              valueStack match {
+                case Nil => v
+                case h :: t =>
+                  curr match {
+                    case Put(key, value) =>
+                      loop(h(()), t, dropCount, errorStack, None, db.updated(key, value))
+                    case _ => loop(h(v), t, dropCount, errorStack, None, db)
+                  }
+              }
+          }
+      }
+    }
+    loop(dbA, List.empty, 0, List.empty, Option.empty, Map.empty).asInstanceOf[A]
+  }
+
+  def main(args: Array[String]): Unit = {
+    // 7:37
+
+    val prog1 = RaiseError[Unit](new Throwable("boom 1"))
+    val prog2 = RaiseError[Unit](new Throwable("boom 2")).handleErrorWith(_ => Pure("hello"))
+    val prog3 = RaiseError[Unit](new Throwable("boom 3")).handleErrorWith(_ => Get("hello"))
+    val prog4 = Pure("hello").flatMap(_ => RaiseError[Unit](new Throwable("boom 4")))
+    val prog5 = Pure("hello")
+      .flatMap(_ => RaiseError[Unit](new Throwable("boom 5")))
+      .map(identity)
+      .handleErrorWith(_ => Pure("world 5"))
+    val prog6 = Pure("hello")
+      .flatMap(_ => RaiseError[Unit](new Throwable("boom 6")))
+      .map(identity)
+      .map(identity)
+      .handleErrorWith(_ => Pure("world 6"))
+
+    val progx = (for {
+      _ <- Put("frank", 4)
+      v <- Get("frank").map(_.getOrElse(0))
+      _ <- Put("foo", 1)
+      v2 <- Get("foo").map(_.getOrElse(3))
+      _ <- RaiseError[Unit](new Throwable("boom"))
+    } yield v + v2).handleErrorWith(_ => Get("frank"))
+
+    println(runDB(prog5.flatMap(_ => prog6)))
+  }
+}
